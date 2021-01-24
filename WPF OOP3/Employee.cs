6cd4cb05ebb45dd17a425d8f0f40cf4ceb3c830a
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-01-24 18:31:46 +0100

    Initial commit

---
+++ WPF OOP3/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_OOP3
+{
+    class Employee
+    {
+
+        private string _firstName;
+        private string _lastName;
+        private double _salary;
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public double Salary { get; set; }
+       
+
+
+
+        public Employee(string firstName, string lastName, double salary)
+        {
+
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Salary = salary;
+            
+        }
+        public override string ToString()
+        {
+            return FirstName.PadRight(15) + LastName.PadRight(15) + Salary.ToString("0.00").PadLeft(8) + " €";
+        }
+
+
+    }
+}
---
+++ WPF OOP3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_OOP3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Employee> myEmployeesList = new List<Employee>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Employee myEmployee = new Employee(txtFirstName.Text, txtLastName.Text, Convert.ToDouble(txtSalary.Text));
+          
+            myEmployeesList.Add(myEmployee);
+            lbResult.ItemsSource = null;
+            lbResult.ItemsSource = myEmployeesList;
+        }
+    }
+}
